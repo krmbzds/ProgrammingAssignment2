648ae0b14dd7c6ee5e2bbf949d4d270c47cb49b6
Kerem Bozdas <krmbzds.github@gmail.com>
2014-04-25 05:31:08 +0300

    Complete peer assessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## Peer Assessment for Coursera (rprog-002)
 
-## Write a short comment describing this function
+## makeCacheMatrix: this function creates a spectial type of matrix that can
+## cache it's inverse. It has it's own set and get functions which helps
+## caching values into the environment it is called (in our case "cacheSolve")
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {     # Use matrix object instead of vector
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above. If the inverse has already been
+## calculated (and the matrix has not changed), then the cachesolve should
+## retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()         # We query for the m value (in the first run it will return NULL)
+    if(!is.null(m)) {           # If the m value is not NULL (i.e. it's in our cache);
+        message("Got result from cache.")
+        return(m)               # Then we return it's value
+    }                           # At this point our function returns if our value is cached, otherwise:
+    data <- x$get()             # We assign the matrix to data variable using the get method
+    m <- solve(data, ...)       # And run the actual calculation using the solve method on 'data' matrix
+    x$setInverse(m)             # Then we cache the value using the set method so we don't calculate it again
+    m                           # We return the result
 }
